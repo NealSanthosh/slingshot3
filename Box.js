b072ee968b5abb0db3059f968755ed9843bb45fa
SansNeal <62467577+SansNeal@users.noreply.github.com>
2020-04-09 13:58:12 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -15,7 +15,7 @@ class Box extends BaseClass {
       push();
       this.Visiblity = this.Visiblity - 5;
       tint(255,this.Visiblity);
-      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+      image(this.image, this.body.position.x, this.body.position.y, 20, 40);
       pop();
     }
     
